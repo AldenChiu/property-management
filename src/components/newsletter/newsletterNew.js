18b6658ab86eda9f6f1d016525d20d4ca0e6a65c
student <aldenchiu13@gmail.com>
2018-07-26 08:43:42 -0600

    Newsletter New Form

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -1,6 +1,6 @@
 import react, { Component } from 'react';
 
-import SigninForm from '../auth/signinForm';
+import NewNewsletterForm from './newsletterNewForm';
 
 class NewNewsletter extends Component {
     render() {
@@ -11,7 +11,7 @@ class NewNewsletter extends Component {
 
         return (
             <div className='new-newsletter'>
-                <SigninForm onSubmit={(event) => this.onSubmit(event)}/>
+                <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
---
+++ src/components/newsletter/newsletterNewForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import { FormTitle } from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import {TextLink} from '../textLink';
+
+class NewNewsletterForm extends Component {
+    render() {
+        
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit = {handleSubmit} className='new-newsletter-form'>
+                <FormTitle className='new-newsletter-form__title' text='Login'/>
+                <Field
+                    className='new-newsletter-form__email'
+                    placeholder="Enter Email"
+                    name='email'
+                    type='email'
+                    title="Email"
+                    component={FormInput}
+                />
+            </form>
+        )
+    }
+}
+
+NewNewsletterForm = reduxForm({
+    form: 'newnewsletter'
+})(NewNewsletterForm);
+
+export default NewNewsletterForm;
